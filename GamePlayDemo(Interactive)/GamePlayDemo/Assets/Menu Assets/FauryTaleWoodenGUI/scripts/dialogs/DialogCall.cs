b2dc2a1f00007e4ed072b413b6508b7ba7e51278
Nick <Nick14.clothier@gmail.com>
2020-05-19 22:57:56 +0100

    Sorting GitHub
    Sorting Required Items Inside of GitHub.
    Keeping Vital Unity Packages Only Now.
    Renaming Items To Allow Outsiders To Understand Projects.

--- GamePlayDemo(Interactive)/GamePlayDemo/Assets/Menu Assets/FauryTaleWoodenGUI/scripts/dialogs/DialogCall.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class DialogCall : MonoBehaviour {
-
-    public void Open() {
-        gameObject.GetComponentInChildren<Dialog>().Open();
-    }
-}
